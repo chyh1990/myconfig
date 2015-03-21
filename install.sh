90658175038888aed327a9f48e5adbaf97dc065d
Yuheng Chen <yuhengchen@sensetime.com>
2015-03-21 14:22:00 +0800

    Add cpplint for vim

--- install.sh
+++ install.sh
@@ -6,7 +6,8 @@ cp -i vimrc ~/.vimrc
 if [[ -d ~/.vim ]]; then
   mv ~/.vim ~/.vim.old
 fi
-cp -r vimdir ~/.vim
+#cp -r vimdir ~/.vim
+ln -s `pwd`/vimdir ~/.vim
 ln -s ~/prog/myconfig/oh-my-zsh ~/.oh-my-zsh
 
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
